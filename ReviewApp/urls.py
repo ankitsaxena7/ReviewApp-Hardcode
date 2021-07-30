c0d13df6cf0af5213924551bc7a16893684c1d2f
Ankit Saxena <ankitsaxena278@gmail.com>
2021-07-30 20:03:50 +0530

    make it better

--- ReviewApp/urls.py
+++ ReviewApp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('review/', include('userReview.urls'))
+    path('', include('userReview.urls'))
 ]
