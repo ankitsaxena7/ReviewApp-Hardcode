097ab8d55229c844bbe4d16c38489d8299c863cd
Ankit Saxena <ankitsaxena278@gmail.com>
2021-07-30 19:39:00 +0530

    make it better

--- ReviewApp/settings.py
+++ ReviewApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'l3k6niu0=)q7iekyijqaz_hmf2_)gd=feaha@=0t=^env-jn-*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['review-avg.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'userReview',
 ]
 
 MIDDLEWARE = [
